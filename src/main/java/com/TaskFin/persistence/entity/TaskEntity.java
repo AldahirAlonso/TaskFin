a01e4f908372ff848343cba39af407b57757ba4b
Aldahir <aldahir0603@hotmail.com>
2026-01-14 22:32:17 -0600

    Creacion de Entity y Implementacion de Lombok

--- src/main/java/com/TaskFin/persistence/entity/TaskEntity.java
+++ src/main/java/com/TaskFin/persistence/entity/TaskEntity.java
@@ -1,6 +1,38 @@
 package com.TaskFin.persistence.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Task_Fin")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false, length = 150, unique = true)
+    private String taskName;
 
+    @Column(nullable = false, length = 150)
+    private String taskDescription;
+
+    @Column(nullable = false, length = 150)
+    private String taskStatus;
+
+    @Column(nullable = false, length = 150)
+    private String taskStartTime;
+
+    @Column(nullable = false, length = 150)
+    private String taskEndTime;
+
+    @Column(nullable = false, length = 150)
+    private String taskStartDate;
+
+    @Column(nullable = false, length = 150)
+    private String taskEndDate;
 }
