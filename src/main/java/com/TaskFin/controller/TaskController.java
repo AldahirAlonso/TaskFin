3c78aa9b3cbd7629c338da2a932ad4ad597b123e
Aldahir <aldahir0603@hotmail.com>
2026-01-15 17:32:20 -0600

    Definiendo el EndPoint Get.

--- src/main/java/com/TaskFin/controller/TaskController.java
+++ src/main/java/com/TaskFin/controller/TaskController.java
@@ -1,9 +1,36 @@
 package com.TaskFin.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.TaskFin.persistence.crud.CrudTaskEntity;
+import com.TaskFin.persistence.entity.TaskEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TaskController {
-    private final CrudTaskEntity crudTaskEntity;
+    private CrudTaskEntity crudTaskEntity;
 
+    public TaskController(CrudTaskEntity crudTaskEntity) {
+        this.crudTaskEntity = crudTaskEntity;
+    }
+
+    @GetMapping("/tasks")
+    public List<TaskEntity> getTasks() {
+        return (List<TaskEntity>) crudTaskEntity.findAll();
+    }
+
+    @PostMapping
+    public String createTask() {
+        return "Task created";
+    }
+
+    @PutMapping
+    public String updateTask() {
+        return "Task updated";
+    }
+
+    @DeleteMapping
+    public String deleteTask() {
+        return "Task deleted";
+    }
 }
